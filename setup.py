d44d12ccc9dc21ed6af26c0a2cad079ad5862b1d
Luke Trenberth <luketrenberth@gmail.com>
2023-06-01 21:44:25 +1200

    Change name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="mtg_management", 
+        name="ManageMTG", 
         version=VERSION,
         author="Luke Trenberth",
         author_email="<luketrenberth@gmail.com>",
