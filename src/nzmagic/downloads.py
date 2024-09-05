8c41d164d90dcf18c9c1d0d5ded62adf63ad250f
Luke Trenberth <luketrenberth@gmail.com>
2024-09-05 23:53:05 +1200

    Create legacy folder

--- src/nzmagic/downloads.py
+++ src/nzmagic/downloads.py
@@ -1,4 +1,5 @@
 import datetime
+import sqlite3
 from pathlib import Path
 
 import pandas as pd
@@ -7,25 +8,37 @@ import requests
 CK_URL = "https://api.cardkingdom.com/api/pricelist"
 
 
-class CardKingdom:
-    def __init__(self):
-        self.name = "ck"
-        module_path = Path(__file__).parent.parent.parent
+DB_NAME = "db.sqlite3"
 
+
+def create_sql(filepath: str):
+
+    # filepath = "downloads/ck/240625.parquet"
+    df = pd.read_parquet(filepath)
+    df.set_index("id", inplace=True)
+    df["date"] = "240625"
+
+    db_connection = sqlite3.connect(DB_NAME)
+    df.to_sql(name="cardkingdom", con=db_connection)
+
+
+class CardKingdom:
+    MODULE_PATH = Path(__file__).parent.parent.parent
+    NAME = "ck"
+
+    def __init__(self):
         # Create Paths if required
-        downloads_path = Path(module_path, "downloads")
+        downloads_path = Path(self.MODULE_PATH, "downloads")
         if not downloads_path.exists():
             downloads_path.mkdir()
 
-        self.base_path = Path(module_path, "downloads", self.name)
+        self.base_path = Path(self.MODULE_PATH, "downloads", self.NAME)
         if not self.base_path.exists():
             self.base_path.mkdir()
 
     def get_data(self):
         """Retrieves data from each URL provided."""
-        new_file = Path(
-            self.base_path, f"{datetime.date.today().strftime('%y%m%d')}.parquet"
-        )
+        new_file = Path(self.base_path, f"{datetime.date.today().strftime('%y%m%d')}.parquet")
         if not new_file.exists():
             json_data = requests.get(CK_URL, timeout=20).json()
             df = pd.DataFrame(json_data["data"])
